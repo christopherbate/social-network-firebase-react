192425987415fe3816daaa9044f4a441b9b39a3c
christopherbate <30323187+christopherbate@users.noreply.github.com>
2017-08-26 12:08:59 -0600

    Login and Signup Forms Stylized

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,21 @@ import React from 'react';
 export default class Login extends React.Component {
   render() {
     return (
-      <div>
-        Stuff here.
+      <div className="SignupContainer">
+
+        <h2>Login to the FB-React Social Network</h2>
+
+        <form onSubmit={this.onSubmit.bind(this)} noValidate className="SignupForm">
+          <input className="SignupForm--input" type="text" ref="email" placeholder="Your email" />
+          <input className="SignupForm--input" type="password" ref="password" placeholder="Password" />
+          <button className="SignupForm--button">Login</button>
+        </form>
+
       </div>
     );
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
   }
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,9 +3,23 @@ import React from 'react';
 export default class Signup extends React.Component {
   render() {
     return (
-      <div>
-        Stuff here.
+      <div className="SignupContainer">
+
+        <h2>Signup for the FB-React Social Network</h2>
+
+        <form onSubmit={this.onSubmit.bind(this)} noValidate className="SignupForm">
+          <input className="SignupForm--input" type="text" ref="username" placeholder="Username" />
+          <input className="SignupForm--input" type="text" ref="email" placeholder="Your email" />
+          <input className="SignupForm--input" type="password" ref="password" placeholder="Password" />
+          <input className="SignupForm--input" type="password" ref="password-confirm" placeholder="Confirm Password" />
+          <button className="SignupForm--button">Signup</button>
+        </form>
+
       </div>
     );
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
   }
 }
