8e7eeda7c18a96cae571d9ece406a731d2d60247
christopherbate <30323187+christopherbate@users.noreply.github.com>
2017-08-26 10:57:20 -0600

    Created all necessary files for React Components.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import {BrowserRouter} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routes = (
+  <BrowserRouter forceRefresh={false}>
+    <App />
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('root'));
 registerServiceWorker();
