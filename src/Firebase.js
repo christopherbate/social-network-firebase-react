31a5e0cbe4a9e38bc4c50783ecb050dcf567d483
christopherbate <30323187+christopherbate@users.noreply.github.com>
2017-08-26 12:24:04 -0600

    Added user signup functionality.

--- src/Firebase.js
+++ src/Firebase.js
@@ -7,3 +7,14 @@ export const app = firebase.initializeApp(FBConfig);
 export const isAuthorized = () => { return false; }
 export const firebaseAuth = firebase.auth();
 export const firebaseDB = firebase.database();
+
+// Signup user.
+export const createFBUser = function(email,password,username,errorCB) {
+  firebase.auth().createUserWithEmailAndPassword(email,password).then( (user) => {
+    if(user){
+        console.log("created user");
+    }
+  }).catch((error)=>{
+    errorCB(error);
+  });
+}
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import {createFBUser} from '../Firebase';
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null
+    }
+
+    // bind the callback.
+    this.handleErrorCallback = this.handleErrorCallback.bind(this);
+  }
+
   render() {
     return (
       <div className="SignupContainer">
@@ -11,15 +23,42 @@ export default class Signup extends React.Component {
           <input className="SignupForm--input" type="text" ref="username" placeholder="Username" />
           <input className="SignupForm--input" type="text" ref="email" placeholder="Your email" />
           <input className="SignupForm--input" type="password" ref="password" placeholder="Password" />
-          <input className="SignupForm--input" type="password" ref="password-confirm" placeholder="Confirm Password" />
+          <input className="SignupForm--input" type="password" ref="passwordconfirm" placeholder="Confirm Password" />
           <button className="SignupForm--button">Signup</button>
         </form>
-
+        {this.renderError()}
       </div>
     );
   }
 
+  renderError() {
+    if(this.state.error) {
+      return(<p>{this.state.error}</p>);
+    }
+
+    return null;
+  }
+
+  handleErrorCallback(error) {
+    this.setState({
+      error:("Error: " + error.code + ": " + error.message)
+    });
+  }
+
   onSubmit(e) {
     e.preventDefault();
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+    let username = this.refs.username.value.trim();
+    let passwordConfirm = this.refs.passwordconfirm.value.trim();
+
+    if(password === passwordConfirm) {
+      createFBUser(email,password,username,this.handleErrorCallback);
+    } else {
+      this.setState( {
+        error:"Passwords do not match"
+      });
+    }
   }
 }
