76820768b6d7638a097edc93f8bacbff824d1129
christopherbate <30323187+christopherbate@users.noreply.github.com>
2017-09-07 05:01:32 -0600

    Added description block to Friends.js

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,3 +1,15 @@
+//------------------------------------------------------------------------
+// Friends.js
+// Author: Christopher Bate
+// Description: Renders the Friends View
+// Friends View consists of an "Add Friend" component and a list of friends.
+// Right now, this component is directly listening to the current user's
+// friends list and provindg updates to the components.
+// TODO: Information for the application state possibly could be transferred
+//        to a central store a'la REDUX.  More to follow.
+// Updates its error state by providing a callback
+// TODO: Change these callbacks to promises.
+//------------------------------------------------------------------------
 import React from 'react';
 import AddFriend from './AddFriend';
 import FriendList from './FriendList';
