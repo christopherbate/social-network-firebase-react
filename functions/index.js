ac7e7a32c4a399dfe5f090eb03429f212ae92de7
christopherbate <30323187+christopherbate@users.noreply.github.com>
2017-09-13 14:59:47 -0600

    Added some documentation for cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,9 @@
+//------------------------------------------------------------------------
+// Index.js -- Cloud Funcitons
+// Author - christopher Bate
+//
+// Description -- implements all the cloud functions for the back-end processing
+//------------------------------------------------------------------------
 const functions = require('firebase-functions');
 var admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
@@ -10,6 +16,11 @@ admin.initializeApp(functions.config().firebase);
 //  response.send("Hello from Firebase!");
 // });
 
+//------------------------------------------------------------------------
+// createUserInfo
+// Runs every time a new user is created, updating their basic information
+// Triggers: createUserEmailEntry, createUsernameTableEntry
+//------------------------------------------------------------------------
 exports.createUserInfo = functions.auth.user().onCreate( event => {
   const user = event.data;
   const email = user.email;
@@ -29,7 +40,14 @@ exports.createUserInfo = functions.auth.user().onCreate( event => {
   });
 });
 
-
+//------------------------------------------------------------------------
+// createUserEmailEntry
+// Runs every time a user is created.
+// TODO: Should run when user updates his email.
+// Creates an entry in the emailToUser node that links back to the user's
+// UID for reverse-lookup
+// Triggers: none
+//------------------------------------------------------------------------
 exports.createUserEmailEntry = functions.auth.user().onCreate( event => {
   const user = event.data;
   var email = user.email;
@@ -49,15 +67,13 @@ exports.createUserEmailEntry = functions.auth.user().onCreate( event => {
   })
 });
 
-exports.processPost = functions.database.ref('/posts/{userID}/{postID}').onWrite(
-  event => {
-    const postInfo = event.data.val();
-    console.log("User" + userID +" posted: " + postInfo);
-
-    // Perform hashtag processing 
-  }
-);
-
+//------------------------------------------------------------------------
+// createUsernameTableEntry
+// Runs everytime a user changes his username or a new username is created.
+// Creates an entry in the usernameToUser Table that links back to the UID
+// for reverse lookup.
+// Triggers: none
+//------------------------------------------------------------------------
 exports.createUsernameTableEntry = functions.database.ref('/userinfo/{userID}/username').onWrite( event => {
 
   var db = admin.database();
@@ -68,9 +84,31 @@ exports.createUsernameTableEntry = functions.database.ref('/userinfo/{userID}/us
   }).then(()=>{
     console.log("usernameToUser table update succeeded.");
   });
-
 });
 
+//------------------------------------------------------------------------
+// processPost
+// Runs everytime a new post is created.
+// Processes that post,creating relevant hash-tag lables and adding that post
+// to relevant users' feed list
+// Triggers:
+//------------------------------------------------------------------------
+exports.processPost = functions.database.ref('/posts/{userID}/{postID}').onWrite(
+  event => {
+    const postInfo = event.data.val();
+    console.log("User" + userID +" posted: " + postInfo);
+
+    // Grab that user's friendsList
+    var friendsRef = db.ref('/userInfo/{userID}/friends');
+    
+  }
+);
+
+//------------------------------------------------------------------------
+// checkNewFriend
+// Runs everytime a user adds a new friend.
+// Checks to see whether that friend is a real user. If not, it deletes the friend.
+//------------------------------------------------------------------------
 exports.checkNewFriend = functions.database.ref('/userinfo/{userID}/friends/{friendID}/username').onWrite(
   event => {
     const newFriendUsername = event.data.val();
