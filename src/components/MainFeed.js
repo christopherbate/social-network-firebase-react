6e79dd64eac4fb1fd2bc52699619dcfbf08a6372
christopherbate <30323187+christopherbate@users.noreply.github.com>
2017-09-13 15:52:28 -0600

    Added postFeed into the mainFeed

--- src/components/MainFeed.js
+++ src/components/MainFeed.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import AddPost from './AddPost';
 import {firebaseDB,firebaseAuth} from '../Firebase';
+import PostFeed from './PostFeed';
 
 export default class MainFeed extends React.Component {
-
-  componentDidMount() {
-    this.friendsListener = firebaseDB.ref('userinfo/' + firebaseAuth.currentUser.uid+'/friends/').orderByChild("username")
-    .on("value", snapshot=> {
-      let friendsList = [];
-      snapshot.forEach( child => {
-        friendsList.push({username:child.val().username});
-      });
-      this.setState({friendsList:friendsList});
-    });
-  }
-
 
   render() {
     return (
       <div>
         <AddPost />
+        <PostFeed friendsList={this.props.friendsList} postList={this.props.postList} />
       </div>
     );
   }
